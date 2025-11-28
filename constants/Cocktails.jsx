20f59a7fbda86f37e55fefe32978083dfa9185b1
alireza <alireza.version@gmail.com>
2025-11-29 00:09:49 +0330

    temp commit

--- constants/Cocktails.jsx
+++ constants/Cocktails.jsx
@@ -4,28 +4,26 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 
 const Cocktails = () => {
+  useGSAP(() => {
+    const parallaxTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#cocktails",
+        start: "top 30%",
+        end: "bottom 80%",
+        scrub: true,
+      },
+    });
 
-    useGSAP(() => {
-        const parallaxTimeline = gsap.timeline({
-            scrollTrigger: {
-                trigger: '#cocktails',
-                start: 'top 30%',
-                end: 'bottom 80%',
-                scrub: true,
-            }
-        })
-
-        parallaxTimeline.from('#c-left-leaf',{
-            x: -100,
-            y: 100,
-
-        })
-        .from('#c-right-leaf',{
-            x: 100,
-            y: 100
-        })
-
-    })
+    parallaxTimeline
+      .from("#c-left-leaf", {
+        x: -100,
+        y: 100,
+      })
+      .from("#c-right-leaf", {
+        x: 100,
+        y: 100,
+      });
+  });
 
   return (
     <div id="cocktails" className="noisy">
